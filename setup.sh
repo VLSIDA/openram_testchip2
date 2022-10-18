dd610d87c5a31e0c5999143bbb9e4da28422b752
mrg <mrg@ucsc.edu>
2022-10-18 14:54:26 -0700

    Commit .gds.gz files and revert OPENLANE_TAG.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 mkdir -p $HOME/mpw8/dependencies2
 export OPENLANE_ROOT=$HOME/mpw8/dependencies2/openlane
 export CARAVEL_ROOT=$HOME/mpw8/dependencies2/caravel
-export PDK_ROOT=$HOME/mpw8/dependencies2/pdk
+export PDK_ROOT=$HOME/mpw8/dependencies2/volare_pdk
+export CARAVEL_LITE=0
 export PDK=sky130B
 
 
